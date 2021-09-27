987c9ba65cbde3c5291f6c3b4e9ccec9325f9b41
than minh hy <thanminhhy@gmail.com>
2021-09-27 11:54:28 +0700

    Add project files.

---
+++ GCD0805AppDev/Controllers/TodosController.cs
@@ -0,0 +1,24 @@
+﻿using GCD0805AppDev.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GCD0805AppDev.Controllers
+{
+    public class TodosController : Controller
+    {
+        // GET: Todos
+        public ActionResult Index()
+        {
+            Todo todo = new Todo()
+            {
+                Id = 1,
+                Description = "Kill Bill",
+                DueDate = new DateTime(2008, 12, 20)
+            };
+            return View(todo);
+        }
+    }
+}
---
+++ GCD0805AppDev/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GCD0805AppDev.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
