61e6fddf9c2fa2a347d3e21b5b0b89b9c8ba3f04
than minh hy <thanminhhy@gmail.com>
2021-10-01 16:56:44 +0700

    Migration CreateTodosTable

---
+++ GCD0805AppDev/Migrations/202110010949452_CreateTodosTable.cs
@@ -0,0 +1,27 @@
+namespace GCD0805AppDev.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTodosTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Todoes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Description = c.String(),
+                        DueDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Todoes");
+        }
+    }
+}
