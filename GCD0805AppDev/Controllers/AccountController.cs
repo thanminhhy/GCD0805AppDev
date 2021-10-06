61aca9a41751c0c438b8784fd1cdd618951d9f72
than minh hy <thanminhhy@gmail.com>
2021-10-06 22:14:41 +0700

    Register a new account as a user

--- GCD0805AppDev/Controllers/AccountController.cs
+++ GCD0805AppDev/Controllers/AccountController.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using GCD0805AppDev.Models;
+using GCD0805AppDev.Utils;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace GCD0805AppDev.Controllers
 {
@@ -22,7 +24,9 @@ namespace GCD0805AppDev.Controllers
         {
         }
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+        public AccountController(
+            ApplicationUserManager userManager, 
+            ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
@@ -155,6 +159,7 @@ namespace GCD0805AppDev.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    await UserManager.AddToRoleAsync(user.Id, Role.User);
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
