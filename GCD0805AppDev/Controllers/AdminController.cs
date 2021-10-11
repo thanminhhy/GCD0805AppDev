7ddd500b98787cc2236a6a9a29a99ba5cef3b8cc
than minh hy <thanminhhy@gmail.com>
2021-10-11 21:00:57 +0700

    CreateManagerAccount(GET)

---
+++ GCD0805AppDev/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using GCD0805AppDev.Utils;
+using GCD0805AppDev.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GCD0805AppDev.Controllers
+{
+    [Authorize(Roles = Role.Admin)]
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext _context;
+        public AdminController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CreateManager()
+        {
+            return View();
+        }
+    }
+}
